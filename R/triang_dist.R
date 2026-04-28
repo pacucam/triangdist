e657435b4252b21b59786109c3315ff7b3e1d39e
pacucam <pacucam@alumni.uv.es>
2026-04-29 01:00:16 +0200

    ptriang function

--- R/triang_dist.R
+++ R/triang_dist.R
@@ -38,3 +38,43 @@ dtriang <- function(x, min, max, mode) {
 
   return(res)
 }
+
+
+#' @title Distribution function for the Triangular Distribution
+#' @param q Numeric vector of quantiles
+#' @param min Lower limit (a)
+#' @param max Upper limit (b)
+#' @param mode Mode (c)
+#' @export
+ptriang <- function(q, min, max, mode) {
+  # 1. Validaciones
+  if (any(min >= max))
+    stop("Error: Min has to be smaller than maximum.")
+  else if (any(mode < min))
+    stop("Error: The mode has to be bigger than the minimum")
+  else if (any(max < mode))
+    stop("Error: The mode has to be smaller than the maximum")
+
+  #Recycling (for vectors)
+  n <- max(length(q), length(min), length(max), length(mode))
+  q <- rep(q, length.out = n)
+  min <- rep(min, length.out = n)
+  max <- rep(max, length.out = n)
+  mode <- rep(mode, length.out = n)
+
+  res <- numeric(n)
+  #First situation: a <= q < c
+  left_part <- q >= min & q < mode
+  res[left_part] <- ((q[left_part] - min[left_part])**2) /
+    ((max[left_part] - min[left_part]) * (mode[left_part] - min[left_part]))
+
+  #Second situation: c <= q <= b
+  right_part <- q >= mode & q <= max
+  res[right_part] <- 1 - ((max[right_part] - q[right_part])^2) /
+    ((max[right_part] - min[right_part]) * (max[right_part] - mode[right_part]))
+
+  #Last situation: q > max
+  res[q > max] = 1
+
+  return(res)
+}
